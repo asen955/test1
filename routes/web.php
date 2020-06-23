87ea71a39df57e1afc198bb13b39293d8f43b823
asen159 <asen159@gmail.com>
2020-06-23 18:01:13 +0530

    dashboard
    dashboard complted

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    return view('layout');
+});
+Route::get('/test1', function () {
+    return view('dashboard');
+});
+
